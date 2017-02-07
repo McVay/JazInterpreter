c84bb9802dd4d69f83ed84733628d8c844e307cb
Dylan <dfmcvay@gmail.com>
2017-02-07 18:18:20 -0500

    init

---
+++ JazInterpreter/JazInterpreter/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Jaz_Interpreter
+{
+    class Program
+    {
+        private static Stack<int> _stack = new Stack<int>();
+        private static KeyValuePair<string, string> _symbolTable = new KeyValuePair<string, string>();
+
+        static void Main(string[] args)
+        {
+            if (!(args.Length > 0))
+            {
+                Console.WriteLine("You must provide a file path.");
+                Environment.Exit(0);
+            }
+
+            //Analyze
+            var lines = File.ReadLines(args[0]);
+            foreach (var line in lines)
+            {
+
+            }
+        }
+    }
+}
